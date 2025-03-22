87585afc3993844ed662d19c9001b0d48e5db147
Ben Tonks <thebentonks@gmail.com>
2025-03-22 17:29:21 +0000

    removed the description of a job from being displayed on the job.index template

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -20,9 +20,6 @@
         </div>
     </div>
 
-    {{-- using {!! !!} interpolation will NOT escape html --}}
-    <p class="text-sm text-slate-500 mb-4">{!! nl2br(e($job->description)) !!}</p>
-
     {{ $slot }}
 
 </x-card>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -2,5 +2,10 @@
 
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'), $job->title => '#']" ></x-breadcrumbs>
 
-    <x-job-card :$job ></x-job-card>
+    <x-job-card :$job >
+        {{-- using {!! !!} interpolation will NOT escape html --}}
+        <p class="text-sm text-slate-500 mb-4">
+            {!! nl2br(e($job->description)) !!}
+        </p>
+    </x-job-card>
 </x-layout>
